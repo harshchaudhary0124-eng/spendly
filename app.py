9908e0e633c439563296ff4baca884ac0bb9687f
harshchaudhary0124-eng <harshchaudhary0124@gmail.com>
2026-06-10 15:51:14 +0530

    added privay policy page and route

--- app.py
+++ app.py
@@ -27,6 +27,11 @@ def terms():
     return render_template("terms.html")
 
 
+@app.route("/policy")
+def policy():
+    return render_template("privacy.html")
+
+
 # ------------------------------------------------------------------ #
 # Placeholder routes — students will implement these                  #
 # ------------------------------------------------------------------ #
